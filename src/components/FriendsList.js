1a724f2178b0a85e6f7b8c257369c7129f8f38af
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-11 19:24:55 +0200

    wip

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -5,14 +5,19 @@ import Friend from './Friend'
 export default function FriendsList(props) {
   // 2- What data does FriendsList need to do its job? Use destructuring
   const { friends, changeStatus } = props
-  
+
   return (
     <div className='list-friends container'>
       {/* 3- We need to loop over the data rendering a Friend as we go */}
       {/* Each friend is going to need a `key` prop and also some other prop with data */}
       {
         friends.map(fr => {
-          return <Friend key={fr.id} />
+          console.log(fr)
+          return <Friend
+            key={fr.id}
+            friend={fr}
+            changeStatus={changeStatus}
+          />
         })
       }
     </div>
