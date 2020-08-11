63031380796a803642cd059edce6b4a3824615d3
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-11 19:59:50 +0200

    wip

--- src/components/PetsList.js
+++ src/components/PetsList.js
@@ -8,6 +8,11 @@ export default function PetsList(props) {
     <div className='list-pets-friends container'>
       {/* 2- Loop over the data generating a Pet element as you go */}
       {/* What props does the Pet component expect? */}
+      {
+        pets.map(pet => {
+          return <div key={pet.id}>{pet.name} is a {pet.type}</div>
+        })
+      }
     </div>
   )
 }
